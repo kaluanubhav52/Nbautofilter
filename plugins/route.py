80dd4af126b28674f6aaf8b15715d42d91ff4be5
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-03-27 21:19:52 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -18,7 +18,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("TG - @SilentXBotz")
+    return web.Response(text="Bot is Alive!", content_type="text/plain")
 
 @routes.get(r"/watch/{path:\S+}", allow_head=True)
 async def stream_handler(request: web.Request):
