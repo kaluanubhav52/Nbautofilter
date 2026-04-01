71daef6fdf9e5b50b56028cb7ce21d32f580f279
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-04-01 22:58:20 +0530

    Change logger level from ERROR to WARNING for hydrogram

--- logging_helper.py
+++ logging_helper.py
@@ -9,7 +9,7 @@ from logging import (
 )
 
 getLogger("requests").setLevel(WARNING)
-getLogger("pyrogram").setLevel(ERROR)
+getLogger("hydrogram").setLevel(ERROR)
 getLogger("aiohttp").setLevel(ERROR)
 getLogger("pymongo").setLevel(WARNING)
 getLogger("aiohttp").setLevel(WARNING)
