5d29a86aa5765fabc24250094a1e5d2c144c22ac
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-04-02 07:56:52 +0530

    Replace pyrogram with hydrogram for FileId import

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -3,7 +3,7 @@ from struct import pack
 import re
 import base64
 from typing import Dict, List, Tuple, Optional
-from pyrogram.file_id import FileId
+from hydrogram.file_id import FileId
 from pymongo.errors import DuplicateKeyError
 from umongo import Instance, Document, fields
 from motor.motor_asyncio import AsyncIOMotorClient
