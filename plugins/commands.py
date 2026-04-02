984b61f209a5c67cd78b592a079aeef384c366f2
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-04-02 07:53:33 +0530

    Replace pyrogram with hydrogram imports

--- plugins/commands.py
+++ plugins/commands.py
@@ -12,10 +12,10 @@ from Script import script
 from datetime import datetime, timedelta
 from database.refer import referdb
 from database.topdb import silentdb
-from pyrogram.enums import ParseMode, ChatType
-from pyrogram import Client, filters, enums
-from pyrogram.errors import FloodWait, ChatAdminRequired
-from pyrogram.types import *
+from hydrogram.enums import ParseMode, ChatType
+from hydrogram import Client, filters, enums
+from hydrogram.errors import FloodWait, ChatAdminRequired
+from hydrogram.types import *
 from database.ia_filterdb import *
 from database.users_chats_db import db
 from info import *
